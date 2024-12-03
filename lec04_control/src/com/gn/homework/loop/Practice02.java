579fe4b3f8c72e7f1586c5033a29da8538959aef
tyk22 <tykkor22@gmail.com>
2024-12-03 17:52:02 +0900

    [kty] 반복문 과제 8번

--- lec04_control/src/com/gn/homework/loop/Practice02.java
+++ lec04_control/src/com/gn/homework/loop/Practice02.java
@@ -4,6 +4,26 @@ import java.util.Scanner;
 
 public class Practice02 {
 	Scanner sc = new Scanner(System.in);
+	
+	public void practice01() {
+		int sum=0;
+		while (1==1) {
+			System.out.print("정수 (1~50) : ");
+			int so = sc.nextInt();
+			if (so > 0) {
+				for(int i =0; i <=so;i++) {
+						
+					if(i%2==0) {
+						sum = sum + i;
+					}
+				}
+				System.out.println(sum);
+				break;
+			}else {
+			System.out.println("1~50 사이의 정수를 입력하세요. ");
+			}
+		}
+	} // 01 종료
 		public void practice02(){
 			
 			//System.out.print("숫자 : ");
@@ -23,18 +43,85 @@ public class Practice02 {
 					for(int i=1;i<=9;i++) {
 						System.out.println(num+" * "+i+" = "+(num*i));
 					}
-					
 				}else if(num>=10) {
 					System.out.println("9 이하의 숫자만 입력해주세요. ");
-				
 				}
 			}
-			
-		}
-}
+		} // 02 종료
 		
-	
-	
-	
+		public void practice04() {
+			do {
+				System.out.println("게임을 실행합니다!");
+				System.out.println("게임을 다시 플레이하시겠습니까?(yes/no)");
+				String yes = sc.next();
+				
+				if(yes.equals("no")){
+					System.out.println("게임을 종료합니다. ");
+					break;
+				}
+			}while(true);
+		}// 04 종료
+		
+		public void practice05() {
+			int sum = 0;
+			while(true) {
+				System.out.print("숫자(0을 입력하면 종료) :");
+				int so = sc.nextInt();
+				if ( so > 0) {
+					sum=sum+so;
+				}else if( so == 0) {
+					System.out.print("양수의 합계 : "+sum);
+					break;
+				}
+			}
+		}// 05 종료
+		
+		public void practice07() {
 
+			while (true) {
+				System.out.print("정수 : ");
+				int ju = sc.nextInt();  
+				if(ju>0) {
+					for(int i=0;i<ju;i++) {
+					if(i%2==0) {
+						System.out.print("박");
+					}else if (i%2==1){
+						System.out.print("수");
+					}
+					}
+					break;
+				}else {
+					System.out.println("양수가 아닙니다. ");
+				}
+			}	
+		} // 07 종료
+	public void practice08() {
+//		문자열 : application
+//		문자 : a
+//		포함된 개수 : 2	
+//		다시 입력하시겠습니까? : y
+//		문자열 : business
+//		문자 : s
+//		포함된 개수 : 3
+//		다시 입력하시겠습니까? : k
+//		잘못된 대답입니다. 다시 입력해주세요.
+//		다시 입력하시겠습니까? : Y
+//		문자열 : ant
+//		문자 : b
+//		포함된 개수 : 0
+//		다시 입력하시겠습니까? : N
+		
+		while(true) {
+			System.out.print("문자열 : ");
+			String str = sc.next();
+			
+			System.out.print("문자 : ");
+			String mon = sc.next();
+			
+			// str.charAt(i)==mon.charAt(0)
+		}
+	}
+	
+	
+}
 
--- lec04_control/src/com/gn/homework/loop/Run.java
+++ lec04_control/src/com/gn/homework/loop/Run.java
@@ -13,13 +13,17 @@ public class Run {
 //		3. 탈출합시다.
 //		6. 트리 만들기
 //		7. 박수박수박
-//		선택: 7
-		System.out.println("했냐");	
+//		선택: 7	
 		System.out.println("실행할 기능을 입력하세요. ");
+		System.out.println("1. 1~50까지 짝수 합하기");
 		System.out.println("2 . 구구단 무한반복");
-		System.out.println("3. 탈출합시다.");
-		System.out.println("6. 트리 만들기");
+		System.out.println("3. 탈출합시다."); // o
+		System.out.println("4. 게임재시작");
+		System.out.println("5. 양수만 더하기"); // o
+		System.out.println("6. 트리 만들기");//o
 		System.out.println("7. 박수박수박");
+		System.out.println("8. 문자열의 개수"); // 해야함
+		
 		System.out.print("선택: ");
 		
 		
@@ -27,7 +31,12 @@ public class Run {
 		Practice02 practice = new Practice02();
 		int number = sc.nextInt();
 		switch(number) {
+		case 1 : practice.practice01();break;
 		case 2 : practice.practice02();break;
+		case 4 : practice.practice04();break;
+		case 5 : practice.practice05();break;
+		case 7 : practice.practice07();break;
+		// case 8 : practice.practice08();break;
 		}
 
 	}
